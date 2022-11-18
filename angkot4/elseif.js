fb95da5fc123d3e2037ed9251baa526a763e91d5
Akbar Ramadhan <akbarramadhaan12@gmail.com>
2022-11-18 22:02:11 +0700

    belajar javascript

--- angkot4/elseif.js
+++ angkot4/elseif.js
@@ -3,10 +3,10 @@ var angkotBeroperasi = 6;
 var noAngkot = 1;
  
 for( var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++){
-    if (noAngkot === 8 || noAngkot === 10 || noAngkot === 5){
-    console.log('Ankot No. '+ noAngkot + ' sedang lembur.' )
-    }else if(  noAngkot <= 6 ) {
+    if(  noAngkot <= 6 && noAngkot !==5) {
     console.log('Angkot No. '+ noAngkot +' Beroperasi dengan baik.');
+    }else if(noAngkot === 8 || noAngkot === 10 || noAngkot === 5){
+        console.log('Ankot No. '+ noAngkot + ' sedang lembur.' )
     }else {
     console.log('Angkot No. '+ noAngkot +' sedang tidak beroperasi.' )
     }
