fa24d9c2abbf6d5f1e30099cf3f7a95af1dc79fa
Akbar Ramadhan <akbarramadhaan12@gmail.com>
2022-12-12 23:00:11 +0700

    splice dan slice

---
+++ Array/slice&splice.js
@@ -0,0 +1,12 @@
+//var buah = ['apple',"mangga","anggur"];
+// 4. splice
+// spice(indexAwal, Maudihapus berapa, elemenBaru1,elemenBaru2 ...)
+// buah.splice(1,1,"strawberry","cerry");
+// console.log(buah.join(' - '));
+
+// 5. Slice
+// slice (awal, akhir)
+var buah = ['apple',"mangga","anggur","pepaya","semangka"];
+var buah2 = buah.slice(1,3);
+console.log(buah.join(' - '));
+console.log(buah2.join(' - '));
