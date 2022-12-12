89494b4df10a96c5aec820da867e4323f221e72c
Akbar Ramadhan <akbarramadhaan12@gmail.com>
2022-12-12 22:29:52 +0700

    array

---
+++ Array/script.js
@@ -0,0 +1,51 @@
+// Manipulasi Array 
+
+//1. Menambah isi array
+// var arr = ["a",1,true];
+// console.log(arr[2]);
+
+// var arr = [];
+// arr[0] = "akbar";
+// arr[1] = "ramadhan";
+// arr[2] = "mahasiswa";
+// arr[6] = "informatika";
+
+// (arr 3,4,5 nya menjadi undefined)
+
+// console.log(arr);
+
+// 2.Menghapus isi array 
+// var buah = ["apple","mangga","pisang","anggur"];
+// buah[1] = undefined;
+
+// console.log(buah);
+
+//3.Menampilkan isi array
+// var buah = ["apple","mangga","anggur", "semongko"];
+
+// for (let i = 0; i < buah.length; i++) {
+//     console.log("nama buah yang " + (i+1) + " bernama buah " + buah[i] + " lejat dan bergiji");    
+// }
+
+
+//Method pada array 
+//1. join
+// var nama = ['Akbar','Ramadhan','Saja'];
+// console.log(nama.join(' - '));
+
+//2. Push dan Pop (ini bagian belakang)
+var buah = ['apple',"mangga","anggur"];
+// buah.push("semongko"); //push buat nambah
+// buah.push('pepaya');   
+// buah.pop();            //pop buat ngurangin
+// buah.pop();
+
+// console.log(buah.join(' - '));
+
+//3. unshift & shift (ini bagian depan)
+// buah.unshift('semongko'); //unshift buat nambah
+// buah.unshift('pepaya');
+// buah.shift();             //shift buat ngurangin
+// buah.shift();
+
+// console.log(buah.join(' - '));
